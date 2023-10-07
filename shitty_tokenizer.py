74ef5211dc2f667ea3ec6377ed2ffaf626fd0bcc
Axel Jacobsen <axelnj44@gmail.com>
2023-10-07 15:48:14 -0700

    move tokenizer to its own file

---
+++ shitty_tokenizer.py
@@ -0,0 +1,64 @@
+import torch
+
+
+from torch.utils.data import Dataset
+
+from typing import Union
+from pathlib import Path
+
+
+class Tokenizer(Dataset):
+    def __init__(self, data_path: Path) -> None:
+        with open(data_path, "r") as f:
+            raw_text = f.read()
+            raw_tokens = sorted(list(set(raw_text)))
+
+        self._tokens = raw_tokens + ["<mask>", "<bos>", "<eos>", "<pad>"]
+        self._token2idx = {token: idx for idx, token in enumerate(raw_tokens)}
+        self._token2idx.update(
+            {
+                "<mask>": len(raw_tokens),
+                "<bos>": len(raw_tokens) + 1,
+                "<eos>": len(raw_tokens) + 2,
+                "<pad>": len(raw_tokens) + 3,
+            }
+        )
+        self._data = raw_text.split("\n")
+        self._max_len = max(len(line) for line in self._data)
+
+    def __repr__(self) -> str:
+        return f"Tokenizer(tokens={''.join(self._tokens)})"
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __getitem__(self, idx: Union[int, slice]) -> torch.Tensor:
+        if isinstance(idx, slice):
+            return torch.stack(
+                [self._get_item(i) for i in range(*idx.indices(len(self)))]
+            )
+        return self._get_item(idx)
+
+    def _get_item(self, idx: int) -> torch.Tensor:
+        """
+        Get a single item from the dataset
+
+        Returns a tensor of shape (max_len + 1), with padding at the
+        end of the actual data, and an <eos> token at the end of the
+        sequence.
+        """
+
+        return torch.tensor(
+            [self._token2idx[t] for t in self._data[idx]]
+            + [self._token2idx["<pad>"]] * (self._max_len - len(self._data[idx]))
+            + [self._token2idx["<eos>"]]
+        )
+
+    def get_tokens(self) -> list[str]:
+        return self._tokens
+
+    def vocab_size(self) -> int:
+        return len(self._tokens)
+
+    def max_size(self) -> int:
+        return self._max_len + 1
--- xfm.py
+++ xfm.py
@@ -4,75 +4,26 @@ import torch
 
 
 from torch import nn
-from torch.utils.data import Dataset
 
-from typing import Union
 from pathlib import Path
 
-
-class Tokenizer(Dataset):
-    def __init__(self, data_path: Path) -> None:
-        with open(data_path, "r") as f:
-            raw_text = f.read()
-            raw_tokens = sorted(list(set(raw_text)))
-
-        self._tokens = raw_tokens + ["<mask>", "<bos>", "<eos>", "<pad>"]
-        self._token2idx = {token: idx for idx, token in enumerate(raw_tokens)}
-        self._token2idx.update(
-            {
-                "<mask>": len(raw_tokens),
-                "<bos>": len(raw_tokens) + 1,
-                "<eos>": len(raw_tokens) + 2,
-                "<pad>": len(raw_tokens) + 3,
-            }
-        )
-        self._data = raw_text.split("\n")
-        self._max_len = max(len(line) for line in self._data)
-
-    def __repr__(self) -> str:
-        return f"Tokenizer(tokens={''.join(self._tokens)})"
-
-    def __len__(self) -> int:
-        return len(self._data)
-
-    def __getitem__(self, idx: Union[int, slice]) -> torch.Tensor:
-        if isinstance(idx, slice):
-            return torch.stack(
-                [self._get_item(i) for i in range(*idx.indices(len(self)))]
-            )
-        return self._get_item(idx)
-
-    def _get_item(self, idx: int) -> torch.Tensor:
-        """
-        Get a single item from the dataset
-
-        Returns a tensor of shape (max_len + 1), with padding at the
-        end of the actual data, and an <eos> token at the end of the
-        sequence.
-        """
-
-        return torch.tensor(
-            [self._token2idx[t] for t in self._data[idx]]
-            + [self._token2idx["<pad>"]] * (self._max_len - len(self._data[idx]))
-            + [self._token2idx["<eos>"]]
-        )
-
-    def get_tokens(self) -> list[str]:
-        return self._tokens
-
-    def vocab_size(self) -> int:
-        return len(self._tokens)
-
-    def max_size(self) -> int:
-        return self._max_len + 1
+from shitty_tokenizer import Tokenizer
 
 
 class Transformer(nn.Module):
     def __init__(
-        self, vocab_size: int, max_sentence_size: int, embedding_dimension_size: int
+        self,
+        vocab_size: int,
+        max_sentence_size: int,
+        embedding_dimension_size: int,
+        attention_dimension_size: int,
     ) -> None:
         super().__init__()
         self._vocab_size = vocab_size
+        self._max_sentence_size = max_sentence_size
+        self._embedding_dimension_size = embedding_dimension_size
+        self._attention_dimension_size = attention_dimension_size
+
         # transposed compared to "formal algorithms"
         self._W_embedding = nn.Linear(vocab_size, embedding_dimension_size, bias=False)
         self._W_positional = nn.Linear(
@@ -80,7 +31,7 @@ class Transformer(nn.Module):
         )
 
     def token_embedding(self, tokens: torch.Tensor) -> torch.Tensor:
-        """ Token embedding of a sequence of tokens
+        """Token embedding of a sequence of tokens
 
         Tokens should be a tensor of shape (batch_size, sequence_length)
         Returns a shape of
@@ -91,8 +42,7 @@ class Transformer(nn.Module):
         )
 
     def positional_embedding(self) -> torch.Tensor:
-        """ Positional embedding of a sequence of tokens
-        """
+        """Positional embedding of a sequence of tokens"""
         return self._W_positional.weight
 
     def embed(self, tokens: torch.Tensor) -> torch.Tensor:
@@ -101,14 +51,24 @@ class Transformer(nn.Module):
         """
         return self.token_embedding(tokens) + self.positional_embedding()
 
+    def basic_attention(self, tokens: torch.Tensor) -> torch.Tensor:
+        embedding = self.embed(tokens)
+
+        return embedding
+
 
 if __name__ == "__main__":
     data_path = Path("canterbury_tales.txt")
     tokenizer = Tokenizer(data_path)
-    transformer = Transformer(tokenizer.vocab_size(), tokenizer.max_size(), 128)
+    transformer = Transformer(
+        tokenizer.vocab_size(),
+        tokenizer.max_size(),
+        embedding_dimension_size=128,
+        attention_dimension_size=128,
+    )
+    batch_size = 16
     print(f"vocab size: {tokenizer.vocab_size()}")
-
-    batch = tokenizer[100:110]
+    batch = tokenizer[100 : 100 + batch_size]
     print(f"sentence shape is {batch.shape=}")
     ret = transformer.token_embedding(batch)
     print(f"token embedding {ret.shape=}")
