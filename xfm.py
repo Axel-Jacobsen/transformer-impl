6c92609fc7776bc082a64f15fafc65d677a44143
Axel Jacobsen <axelnj44@gmail.com>
2023-10-09 13:33:18 -0700

    refactor embeddign out of attention

--- xfm.py
+++ xfm.py
@@ -5,47 +5,93 @@ import torch
 
 from torch import nn
 
+from pathlib import Path
 
-# def basic_tests():
-#     data_path = Path("canterbury_tales.txt")
-#     tokenizer = Tokenizer(data_path, pad=True)
-#     attention = Attention(
-#         tokenizer.vocab_size(),
-#         tokenizer.max_size(),
-#         embedding_dimension_size=32,
-#         attention_dimension_size=32,
-#         out_dimension_size=32,
-#     )
-#     batch_size = 1
+from tokenizer import Tokenizer
+from attention import MultiHeadAttention, Attention, Embedding
 
-#     print(f"vocab size: {tokenizer.vocab_size()}")
 
-#     batch = tokenizer[100 : 100 + batch_size]
-#     print(tokenizer._data[100])
+"""
+class MultiHeadAttention(nn.Module):
+    def __init__(
+        self,
+        vocab_size: int,
+        max_sentence_size: int,
+        num_heads: int,
+        mid_dim_size: int,
+        embedding_dimension_size: int,
+        attention_dimension_size: int,
+        out_dimension_size: int,
+    ) -> None:
+        super().__init__()
+        self._num_heads = num_heads
+        self._mid_dim_size = mid_dim_size
+        self._vocab_size = vocab_size
+        self._max_sentence_size = max_sentence_size
+        self._embedding_dimension_size = embedding_dimension_size
+        self._attention_dimension_size = attention_dimension_size
+        self._out_dimension_size = out_dimension_size
+"""
 
-#     print(f"sentence shape is {batch.shape=}")
 
-#     attention.token_embedding(batch)
+class DTransformer(nn.Module):
+    def __init__(
+        self,
+        num_heads: int,
+        mid_dim_size: int,
+        vocab_size: int,
+        max_sentence_size: int,
+        embedding_dim_size: int,
+        attention_dim_size: int,
+        out_dim_size: int,
+        num_layers: int,
+    ):
+        for l in range(num_layers):
+            pass
 
-#     attention.basic_single_query_attention(batch[0, 11], list(batch[0, :11]))
-#     fin_normal = attention.attention(batch[0], batch[0])
-#     print(f"{fin_normal.shape=}")
 
-#     mh_attention = MultiHeadAttention(
-#         tokenizer.vocab_size(),
-#         tokenizer.max_size(),
-#         4,
-#         32,
-#         embedding_dimension_size=32,
-#         attention_dimension_size=32,
-#         out_dimension_size=32,
-#     )
+def basic_tests():
+    data_path = Path("canterbury_tales.txt")
+    tokenizer = Tokenizer(data_path, pad=True)
 
-#     fin_mh = mh_attention(batch[0], batch[0])
-#     print(f"{fin_mh.shape=}")
+    embedder = Embedding(
+        vocab_size=tokenizer.vocab_size(),
+        max_sentence_size=tokenizer.max_size(),
+        embedding_dimension_size=32,
+    )
+
+    attention = Attention(
+        tokenizer.vocab_size(),
+        tokenizer.max_size(),
+        embedding_dimension_size=32,
+        attention_dimension_size=32,
+        out_dimension_size=32,
+    )
+
+    batch_size = 1
+    batch = tokenizer[100 : 100 + batch_size]
+
+    attention.basic_single_query_attention(
+        embedder.embed(batch[0, 11]), [embedder.embed(b) for b in batch[0, :11]]
+    )
+    fin_normal = attention.attention(embedder.embed(batch[0]), embedder.embed(batch[0]))
+
+    mh_attention = MultiHeadAttention(
+        tokenizer.vocab_size(),
+        tokenizer.max_size(),
+        4,
+        32,
+        embedding_dimension_size=32,
+        attention_dimension_size=32,
+        out_dimension_size=32,
+    )
+
+    fin_mh = mh_attention(embedder.embed(batch[0]), embedder.embed(batch[0]))
 
 
 if __name__ == "__main__":
+    basic_tests()
+
     from itertools import cycle
 
     seq = cycle("aab")
