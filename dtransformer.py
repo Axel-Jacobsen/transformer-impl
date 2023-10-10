63cd247ef09e97d2a37250fd5f8c28fdfb6b774c
Axel Jacobsen <axelnj44@gmail.com>
2023-10-10 16:35:02 -0700

    replace my bad embedding with good embedding

--- dtransformer.py
+++ dtransformer.py
@@ -7,7 +7,6 @@ from torch import nn
 from typing import Optional
 from dataclasses import dataclass
 
-from embed import Embedding
 from attention import MultiHeadAttention, generate_square_subsequent_mask
 
 
@@ -37,30 +36,17 @@ class DTransformer(nn.Module):
         super().__init__()
         self.num_layers = num_layers
         self.mlp_dim_size = mlp_dim_size
-        self.embedder = Embedding(**embedding_params.__dict__)
+        self.embedder = nn.Embedding(
+            embedding_params.vocab_size, embedding_params.embedding_dimension_size
+        )
         self.layer_norm = [
             (
-                nn.LayerNorm(
-                    [
-                        embedding_params.max_sentence_size,
-                        embedding_params.embedding_dimension_size,
-                    ]
-                ),
-                nn.LayerNorm(
-                    [
-                        embedding_params.max_sentence_size,
-                        embedding_params.embedding_dimension_size,
-                    ]
-                ),
+                nn.LayerNorm(embedding_params.embedding_dimension_size),
+                nn.LayerNorm(embedding_params.embedding_dimension_size),
             )
             for _ in range(num_layers)
         ]
-        self.final_layer_norm = nn.LayerNorm(
-            [
-                embedding_params.max_sentence_size,
-                embedding_params.embedding_dimension_size,
-            ]
-        )
+        self.final_layer_norm = nn.LayerNorm(embedding_params.embedding_dimension_size)
         self.attention = [
             MultiHeadAttention(
                 **attention_params.__dict__,
@@ -76,9 +62,6 @@ class DTransformer(nn.Module):
             for _ in range(num_layers)
         ]
         self.gelu = nn.GELU()
-        self.causal_mask = generate_square_subsequent_mask(
-            embedding_params.max_sentence_size,
-        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """x: (batch_size, max_sentence_size)
@@ -89,12 +72,15 @@ class DTransformer(nn.Module):
         ugly!
         """
         x = self.embedder(x)
+        sentence_len, embedding_dim = x.shape
+        causal_mask = generate_square_subsequent_mask(sentence_len, device=x.device)
         for n in range(self.num_layers):
             x = self.layer_norm[n][0](x)
-            x = x + self.attention[n](x, x, mask=self.causal_mask)
+            x = x + self.attention[n](x, x, mask=causal_mask)
             x = self.layer_norm[n][1](x)
             x = x + self.mlp[n][1](self.gelu(self.mlp[n][0](x)))
 
         x = self.final_layer_norm(x)
-        x = self.embedder.unembed(x)
-        return nn.functional.softmax(x, dim=-1)
+        if self.training:
+            return nn.functional.softmax(x, dim=-1)
+        return x
