e4a9fb1d27c28610856e0a33baf7b8f17513ba9d
Axel Jacobsen <axelnj44@gmail.com>
2023-10-10 17:16:32 -0700

    refactor DTransformer into layers

--- dtransformer.py
+++ dtransformer.py
@@ -25,6 +25,45 @@ class MultiHeadAttentionParams:
     num_heads: Optional[int] = None
 
 
+class DTransformerLayer(nn.Module):
+    def __init__(
+        self,
+        mlp_dim_size: int,
+        embedding_params: EmbeddingParams,
+        attention_params: MultiHeadAttentionParams,
+    ):
+        super().__init__()
+        self.mlp_dim_size = mlp_dim_size
+
+        self.layer_norm_1 = nn.LayerNorm(embedding_params.embedding_dimension_size)
+        self.layer_norm_2 = nn.LayerNorm(embedding_params.embedding_dimension_size)
+        self.layer_norm_3 = nn.LayerNorm(embedding_params.embedding_dimension_size)
+        self.attention = MultiHeadAttention(
+            **attention_params.__dict__,
+            **embedding_params.__dict__,
+        )
+        self.mlp_1 = nn.Linear(embedding_params.embedding_dimension_size, mlp_dim_size)
+        self.mlp_2 = nn.Linear(mlp_dim_size, embedding_params.embedding_dimension_size)
+        self.gelu = nn.GELU()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """x: (batch_size, max_sentence_size)
+
+        x is a batch of sequences of tokens
+        returns a batch of sequences of embeddings
+        """
+        sentence_len, embedding_dim = x.shape
+        causal_mask = generate_square_subsequent_mask(sentence_len, device=x.device)
+
+        x = self.layer_norm_1(x)
+        x = x + self.attention(x, x, mask=causal_mask)
+        x = self.layer_norm_2(x)
+        x = x + self.mlp_2(self.gelu(self.mlp_1(x)))
+        x = self.layer_norm_3(x)
+
+        return x
+
+
 class DTransformer(nn.Module):
     def __init__(
         self,
@@ -35,52 +74,24 @@ class DTransformer(nn.Module):
     ):
         super().__init__()
         self.num_layers = num_layers
-        self.mlp_dim_size = mlp_dim_size
         self.embedder = nn.Embedding(
             embedding_params.vocab_size, embedding_params.embedding_dimension_size
         )
-        self.layer_norm = [
-            (
-                nn.LayerNorm(embedding_params.embedding_dimension_size),
-                nn.LayerNorm(embedding_params.embedding_dimension_size),
-            )
-            for _ in range(num_layers)
-        ]
-        self.final_layer_norm = nn.LayerNorm(embedding_params.embedding_dimension_size)
-        self.attention = [
-            MultiHeadAttention(
-                **attention_params.__dict__,
-                **embedding_params.__dict__,
-            )
-            for _ in range(num_layers)
-        ]
-        self.mlp = [
-            (
-                nn.Linear(embedding_params.embedding_dimension_size, mlp_dim_size),
-                nn.Linear(mlp_dim_size, embedding_params.embedding_dimension_size),
-            )
-            for _ in range(num_layers)
-        ]
-        self.gelu = nn.GELU()
+        self.model = nn.Sequential(
+            *[
+                DTransformerLayer(mlp_dim_size, embedding_params, attention_params)
+                for _ in range(num_layers)
+            ]
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """x: (batch_size, max_sentence_size)
 
         x is a batch of sequences of tokens
         returns a batch of sequences of embeddings
-
-        ugly!
         """
         x = self.embedder(x)
-        sentence_len, embedding_dim = x.shape
-        causal_mask = generate_square_subsequent_mask(sentence_len, device=x.device)
-        for n in range(self.num_layers):
-            x = self.layer_norm[n][0](x)
-            x = x + self.attention[n](x, x, mask=causal_mask)
-            x = self.layer_norm[n][1](x)
-            x = x + self.mlp[n][1](self.gelu(self.mlp[n][0](x)))
-
-        x = self.final_layer_norm(x)
+        x = self.model(x)
         if self.training:
             return nn.functional.softmax(x, dim=-1)
         return x
